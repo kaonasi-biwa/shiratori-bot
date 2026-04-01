371f9a31d7fec47550595fd5958cad6b1997f143
kaonasi-biwa <85387717+kaonasi-biwa@users.noreply.github.com>
2026-04-01 17:44:12 +0900

    statusコマンドの追加

--- commands/index.ts
+++ commands/index.ts
@@ -1,8 +1,10 @@
 import { pingCommand } from "./ping/index.ts";
 import { exitCommand } from "./exit/index.ts";
+import { statusCommand } from "./status/index.ts";
 import type { Command } from "./command.d.ts";
 
 export const Commands: Command[] = [
   pingCommand,
   exitCommand,
+  statusCommand,
 ]
---
+++ commands/status/index.ts
@@ -0,0 +1,37 @@
+import { CommandInteraction } from "discord.js";
+import type { Command } from "../command.d.ts"
+import { cpus, freemem, totalmem } from "node:os"
+
+export const statusCommand: Command = {
+  data: {
+    name: "status",
+    description: "CPUとメモリの確認"
+  },
+  async execute(interaction : CommandInteraction) {
+    await interaction.deferReply()
+    let cpuUnued = 0;
+    let cpuTotal = 0;
+    {
+      const cpuInfos = cpus()
+      for(const cpuinfo of cpuInfos){
+        cpuUnued += cpuinfo.times.idle;
+        for(const usedType in cpuinfo.times) cpuTotal += cpuinfo.times[usedType as "user"] ?? 0
+      }
+    }
+    await new Promise((resolve) => setTimeout(resolve, 10000))
+    {
+      const cpuInfos = cpus()
+      for(const cpuinfo of cpuInfos){
+        cpuUnued -= cpuinfo.times.idle;
+        for(const usedType in cpuinfo.times) cpuTotal -= cpuinfo.times[usedType as "user"] ?? 0
+      }
+    }
+    await interaction.reply(`今の状態はこんな感じだよ
+\`\`\`
+現在時刻     : ${new Date()}
+CPU使用率    : ${Math.floor((cpuTotal - cpuUnued) * 100 / cpuTotal)}
+メモリ使用率 : ${Math.floor((totalmem() - freemem()) * 100 / totalmem())}
+\`\`\`
+`)
+  }
+}
