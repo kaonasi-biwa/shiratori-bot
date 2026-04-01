f0addef00b20f9fc2e92edf21bf3deadd20f9fdb
kaonasi-biwa <85387717+kaonasi-biwa@users.noreply.github.com>
2026-04-01 18:02:15 +0900

    変数名を変更

--- commands/status/index.ts
+++ commands/status/index.ts
@@ -9,12 +9,12 @@ export const statusCommand: Command = {
   },
   async execute(interaction : CommandInteraction) {
     await interaction.deferReply()
-    let cpuUnued = 0;
+    let cpuUnused = 0;
     let cpuTotal = 0;
     {
       const cpuInfos = cpus()
       for(const cpuinfo of cpuInfos){
-        cpuUnued += cpuinfo.times.idle;
+        cpuUnused += cpuinfo.times.idle;
         for(const usedType in cpuinfo.times) cpuTotal += cpuinfo.times[usedType as "user"] ?? 0
       }
     }
@@ -22,14 +22,14 @@ export const statusCommand: Command = {
     {
       const cpuInfos = cpus()
       for(const cpuinfo of cpuInfos){
-        cpuUnued -= cpuinfo.times.idle;
+        cpuUnused -= cpuinfo.times.idle;
         for(const usedType in cpuinfo.times) cpuTotal -= cpuinfo.times[usedType as "user"] ?? 0
       }
     }
     await interaction.editReply(`今の状態はこんな感じだよ
 \`\`\`
 現在時刻     : ${new Date()}
-CPU使用率    : ${Math.floor(100 - cpuUnued * 100 / cpuTotal)}%
+CPU使用率    : ${Math.floor(100 - cpuUnused * 100 / cpuTotal)}%
 メモリ使用率 : ${Math.floor(100 - freemem() * 100 / totalmem())}%
 \`\`\`
 `)
